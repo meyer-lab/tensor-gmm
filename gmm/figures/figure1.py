69936a3f1f2e4e14879706261cf9b6f8126333b6
Aaron Meyer <ameyer@ucla.edu>
2022-02-25 08:21:47 -0800

    Some cleanup

--- gmm/figures/figure1.py
+++ gmm/figures/figure1.py
@@ -1,19 +1,13 @@
 """
 This creates Figure 1.
 """
-import enum
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy import stats
-from sklearn.decomposition import PCA
-from pathlib import Path
 
 
 from .common import subplotLabel, getSetup
-from ..imports import importflowDF, smallDF
-from ..GMM import cvGMM, runPCA, probGMM
+from ..imports import smallDF
+from ..GMM import cvGMM
 
 
 def makeFigure():
@@ -28,7 +22,6 @@ def makeFigure():
 
     # smallDF(Amount of cells wanted per experiment); 336 conditions in total
     cellperexp = 50
-    uniq = 336
     zflowDF, experimentalcells = smallDF(cellperexp)
 
     ax[0].hist(experimentalcells, bins=20)
@@ -36,19 +29,11 @@ def makeFigure():
     ylabel = "Events"
     ax[0].set(xlabel=xlabel, ylabel=ylabel)
 
-    # runPCA(Runs PCA on DF with output [PCs,VarianceExplained])
-    components, vcexplained = runPCA(zflowDF)
-
-    ax[1].scatter(components, vcexplained)
-    xlabel = "Principal Components"
-    ylabel = "Variance"
-    ax[1].set(xlabel=xlabel, ylabel=ylabel)
-
     # scoreDF(Determining rand_score and score for GMM with on DF and max cluster # with output [DF(Cluster #,Score)])
     maxcluster = 18
     scoreDF = cvGMM(zflowDF, maxcluster)
 
-    for i in range(len(components)):
+    for i in range(maxcluster):
         ax[2].plot(scoreDF.Cluster.values, scoreDF.rand_score.values)
         ax[3].plot(scoreDF.Cluster.values, scoreDF.ll_score.values)
 
@@ -56,27 +41,5 @@ def makeFigure():
     ylabel = "Score"
     ax[2].set(xlabel=xlabel, ylabel=ylabel)
     ax[3].set(xlabel=xlabel, ylabel=ylabel)
-
-    # probGMM(Runs PCA on DF with output [PCs,VarianceExplained])
-    nk, means, covariances = probGMM(zflowDF, maxcluster, cellperexp)
-
-    statDF = []
-
-    for i in range(uniq):
-        concentration = zflowDF["Dose"].iloc[i * cellperexp]
-        ligand = zflowDF["Ligand"].iloc[i * cellperexp]
-        valency = zflowDF["Valency"].iloc[i * cellperexp]
-        time = zflowDF["Time"][i * cellperexp]
-        for j in range(maxcluster):
-            ave_stat = means[i, j, :]
-            statDF.append([time, ligand, valency, concentration, j + 1, ave_stat[0], ave_stat[1], ave_stat[2], ave_stat[3], ave_stat[4]])
-
-    statDF = pd.DataFrame(statDF, columns=["Time", "Ligand", "Valency", "Concentration", "Cluster", "Foxp3", "CD25", "CD4", "CD45RA", "pSTAT5"])
-
-    sns.scatterplot(data=statDF, x="Concentration", y="pSTAT5", hue="Cluster", ax=ax[4], style="Ligand")
-    ax[4].legend(title="Cluster", loc="best")
-    xlabel = "Concentration"
-    ylabel = "pSTAT5"
-    ax[4].set(xlabel=xlabel, ylabel=ylabel,xscale="log")
 
     return f
--- gmm/figures/figure2.py
+++ gmm/figures/figure2.py
@@ -1,19 +1,14 @@
 """
 This creates Figure 2.
 """
-import enum
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-from scipy import stats
-from sklearn.decomposition import PCA
-from pathlib import Path
 
 
 from .common import subplotLabel, getSetup
-from ..imports import importflowDF, smallDF
-from ..GMM import cvGMM, runPCA, probGMM
+from ..imports import smallDF
+from ..GMM import probGMM
 
 
 def makeFigure():
@@ -21,52 +16,25 @@ def makeFigure():
     # Get list of axis objects
     ax, f = getSetup((8, 4), (2, 3))
 
-    # ax[5].axis("off")
-
     # Add subplot labels
     subplotLabel(ax)
 
     # smallDF(Amount of cells wanted per experiment); 336 conditions in total
     cellperexp = 6000
-    uniq = 336
-    zflowDF, experimentalcells = smallDF(cellperexp)
+    zflowDF, _ = smallDF(cellperexp)
 
-    ax[0].hist(experimentalcells, bins=20)
-    xlabel = "Number of Cells per Experiment"
-    ylabel = "Events"
-    ax[0].set(xlabel=xlabel, ylabel=ylabel)
+    maxcluster = 5
+    _, means, _ = probGMM(zflowDF, maxcluster, cellperexp)
 
-    # runPCA(Runs PCA on DF with output [PCs,VarianceExplained])
-    components, vcexplained = runPCA(zflowDF)
+    statMeans = means[:, :, 3]  # Expt x cluster
 
-    ax[1].scatter(components, vcexplained)
-    xlabel = "Principal Components"
-    ylabel = "Variance"
-    ax[1].set(xlabel=xlabel, ylabel=ylabel)
+    meansDF = zflowDF.iloc[::cellperexp, :]  # Subset to one row per expt
+    meansDF = meansDF[["Time", "Ligand", "Valency", "Dose"]]  # Only keep descriptive rows
+    meansDF = pd.concat([meansDF] * maxcluster, ignore_index=True) # Duplicate for each cluster
+    meansDF["Cluster"] = np.repeat(np.arange(1, maxcluster + 1), repeats=statMeans.shape[0]) # Track clusters
+    meansDF["pSTAT5"] = statMeans.flatten(order="F")  # Checked the ordering here
 
-    maxcluster = 18
+    sns.scatterplot(data=meansDF, x="Dose", y="pSTAT5", hue="Cluster", ax=ax[4], style="Ligand")
+    ax[4].set(xscale="log")
 
-    # probGMM(Runs PCA on DF with output [PCs,VarianceExplained])
-    nk, means, covariances = probGMM(zflowDF, maxcluster, cellperexp)
-
-    statDF = []
-
-    for i in range(uniq):
-        concentration = zflowDF["Dose"].iloc[i * cellperexp]
-        ligand = zflowDF["Ligand"].iloc[i * cellperexp]
-        valency = zflowDF["Valency"].iloc[i * cellperexp]
-        time = zflowDF["Time"][i * cellperexp]
-        for j in range(maxcluster):
-            ave_stat = means[i, j, :]
-            statDF.append([time, ligand, valency, concentration, j + 1, ave_stat[0], ave_stat[1], ave_stat[2], ave_stat[3], ave_stat[4]])
-
-    statDF = pd.DataFrame(statDF, columns=["Time", "Ligand", "Valency", "Concentration", "Cluster", "Foxp3", "CD25", "CD4", "CD45RA", "pSTAT5"])
-
-    sns.scatterplot(data=statDF,x="Concentration",y="pSTAT5", hue="Cluster",ax=ax[4], style="Ligand")
-    ax[2].legend(title = "Cluster", loc = 'best')
-    xlabel = "Concentration"
-    ylabel = "pSTAT5"
-    ax[2].set(xlabel=xlabel, ylabel=ylabel,xscale="log")
-
-    # statDF.to_csv('output.csv')
     return f
