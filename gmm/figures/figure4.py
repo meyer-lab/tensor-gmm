3e3e2671d843d04774dbcedfee8d69626d478f22
Aaron Meyer <ameyer@ucla.edu>
2022-03-16 08:40:46 -0700

    Fix

--- gmm/figures/figure4.py
+++ gmm/figures/figure4.py
@@ -14,7 +14,7 @@ from ..tensor import tensor_decomp, tensor_means
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((20, 30), (2, 2))
+    ax, f = getSetup((20, 30), (2, 3))
 
     # Add subplot labels
     subplotLabel(ax)
