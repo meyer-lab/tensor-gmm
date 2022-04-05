b976ec3b79d2a5780f9de37a08365fd8eb67a378
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2022-04-05 06:55:11 -0700

    Update GMM.py

--- gmm/GMM.py
+++ gmm/GMM.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import xarray as xa
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
