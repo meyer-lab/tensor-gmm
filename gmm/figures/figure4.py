040d004ce5c6209ef80a7fb758ad1a1f05de7958
Aaron Meyer <ameyer@ucla.edu>
2022-05-05 06:36:17 -0700

    Remove Fig. 4

--- gmm/figures/figure4.py
+++ gmm/figures/figure4.py
@@ -1,16 +1,23 @@
 """
-This creates Figure 4.
+This creates Figure 5.
 """
 import numpy as np
 import tensorly as tl
+import pandas as pd
+import seaborn as sns
 from scipy.optimize import minimize
 from jax.config import config
 from jax import value_and_grad
 from .common import subplotLabel, getSetup
-from gmm.imports import smallDF
-from gmm.GMM import probGMM
-from gmm.tensor import tensor_decomp, cp_to_vector, maxloglik
+from ..imports import smallDF
+from ..GMM import probGMM
+from ..tensor import tensor_decomp, cp_to_vector, maxloglik, tensorcovar_decomp, vector_to_cp, pt_to_vector, maxloglik_ptnnp, vector_to_pt
 from tensorly.decomposition import partial_tucker
+from tensorly.tucker_tensor import tucker_to_vec
+from tensorly.tenalg import multi_mode_dot
+from numpy.testing import assert_allclose
+import jax.numpy as jnp
+
 
 config.update("jax_enable_x64", True)
 
@@ -18,7 +25,7 @@ config.update("jax_enable_x64", True)
 def makeFigure():
     """Get a list of the axis objects and create a figure."""
     # Get list of axis objects
-    ax, f = getSetup((10, 8), (3, 2))
+    ax, f = getSetup((10, 8), (2, 2))
 
     # Add subplot labels
     subplotLabel(ax)
@@ -31,26 +38,33 @@ def makeFigure():
     # probGM(Xarray, max cluster): Xarray [nk, means, covar] while using estimation gaussian parameters
     # tMeans[Cluster, Marker, Time, Dose, Ligand]
     maxcluster = 3
-    nk, tMeans, tCovar = probGMM(zflowTensor, maxcluster)
+    nk, tMeans, tPrecision = probGMM(zflowTensor, maxcluster)
 
-    # tensor_decomp(tensor means, rank, type of decomposition):
-    # [DF,tensorfactors/weights] creates DF of factors for different
+    # tensorcovar_decomp(precision, rank, nk):
+    # [DF,core tensor, tensorfactors] creates DF of factors for different
     # conditions and output of decomposition
 
     ranknumb = 2
-    _, facInfo = tensor_decomp(tMeans, ranknumb)
+    _, facInfo = tensor_decomp(tMeans, ranknumb,"NNparafac")
+   
+    ptFactors, ptCore = tensorcovar_decomp(tPrecision, ranknumb, nk)
+
+    vecptFacCore, ptFacLength = pt_to_vector(ptFactors,ptCore)
 
     nkValues = np.exp(np.nanmean(np.log(nk), axis=(1, 2, 3)))
     cpVector = cp_to_vector(facInfo)
-    cpVector = np.concatenate((nkValues, cpVector))
-    args = (facInfo, tCovar, zflowTensor)
+    cpVectorLength = len(cpVector)
+    totalVector = np.concatenate((nkValues, cpVector, vecptFacCore))
+
+    args = (tPrecision, facInfo, zflowTensor, cpVectorLength, ptFacLength, ptCore)
 
     tl.set_backend("jax")
 
-    func = value_and_grad(maxloglik)
+    func = value_and_grad(maxloglik_ptnnp)
 
-    opt = minimize(func, cpVector, jac=True, method="L-BFGS-B", args=args, options={"iprint": 50})
+    opt = minimize(func, totalVector, jac=True, method="L-BFGS-B", args=args, options={"iprint": 50})
 
     tl.set_backend("numpy")
+
 
     return f
