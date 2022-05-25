b80bfe0c6c913bbd8c2f9c862253dacb28b69451
Aaron Meyer <ameyer@ucla.edu>
2022-05-25 07:42:19 -0700

    Setup

--- gmm/imports.py
+++ gmm/imports.py
@@ -1,6 +1,7 @@
 """ Methods for data import and normalization. """
 
 import numpy as np
+import pyarrow as pa
 import pyarrow.parquet as pq
 
 
@@ -9,26 +10,32 @@ def smallDF(numCells: int):
     Zscores all markers per experiment but pSTAT5 normalized over all experiments
     Outputs amount of experiments and cell types as an Xarray"""
     # numCells = Amount of cells per experiment
-    flowDF = importflowDF()
-    gVars = ["Time", "Dose", "Ligand"]
+    flowArrow = importflowDF()
+    gVars = ["Time", "Dose", "Ligand", "Valency"]
     # Columns that should be trasformed
     tCols = ["Foxp3", "CD25", "CD45RA", "CD4"]
     transCols = tCols + ["pSTAT5"]
 
-    flowDF["Ligand"] = flowDF["Ligand"] + "-" + flowDF["Valency"].astype(int).astype(str)
-    flowDF.drop(columns=["Valency", "index", "Date"], axis=1, inplace=True)
-
     # Data was measured for CD3/CD8/CD56 was not measured for non-Tregs/Thelpers
     # Also drop columns with missing values
-    flowDF = flowDF.dropna(subset=["Foxp3"]).dropna(axis=1)
+    flowArrow = flowArrow.filter(pa.compute.is_finite(flowArrow["Foxp3"]))
+    flowArrow = flowArrow.select(transCols + gVars + ["Cell Type"])
+    flowDF = flowArrow.to_pandas()
+
+    # Group and subset
     experimentcells = flowDF.groupby(by=gVars).size()
     flowDF[tCols] = flowDF.groupby(by=gVars)[tCols].transform(lambda x: x / np.std(x))  # Dividing by std per experiement
     for mark in transCols:
         flowDF = flowDF[flowDF[mark] < flowDF[mark].quantile(0.995)]  # Getting rid of outlier values
     flowDF = flowDF.groupby(by=gVars).sample(n=numCells).reset_index(drop=True)
+
+    # Add valency to the name
+    flowDF["Ligand"] = flowDF["Ligand"] + "-" + flowDF["Valency"].apply(lambda x: f'{x:.0f}')
+    flowDF.drop(columns=["Valency"], axis=1, inplace=True)
+
     flowDF["Cell Type"] = flowDF["Cell Type"].replace({"None": 1, "Treg": 2, "Thelper": 3})
     flowDF["pSTAT5"] /= np.std(flowDF["pSTAT5"])  # For pSTAT5 only, dividing my std of all experiments
-    flowDF.sort_values(by=gVars, inplace=True)
+    flowDF.sort_values(by=["Time", "Dose", "Ligand"], inplace=True)
 
     flowDF["Cell"] = np.tile(np.arange(1, numCells + 1), int(flowDF.shape[0] / numCells))
     flowDF = flowDF.set_index(["Cell", "Time", "Dose", "Ligand"]).to_xarray()
@@ -43,8 +50,6 @@ def smallDF(numCells: int):
 def importflowDF():
     """Downloads all conditions, surface markers and cell types.
     Cells are labeled via Thelper, None, Treg, CD8 or NK"""
-
     monomeric = pq.read_table("/opt/andrew/FlowDataGMM_Mon_NoSub.pq")
-    monomeric = monomeric.to_pandas()
-
-    return monomeric
+    dimeric = pq.read_table("/opt/andrew/FlowDataGMM_Mon_NoSub.pq")
+    return pa.concat_tables([monomeric, dimeric])
