b1e061304c2ab931d9a3a0f2ae515cbe3471971d
Andrew Ramirez <aramirez@aretha.seas.ucla.edu>
2022-07-18 16:28:42 -0700

    Allowing minimization function to vary NK for Popalign dataset

--- gmm/figures/figure7.py
+++ gmm/figures/figure7.py
@@ -20,7 +20,7 @@ def makeFigure():
     # geneDF = gene_import(offset=1.1,filter=True)
 
     num = 290
-    fac = 12
+    fac = 3
     drugXA, fac_vector, sse = ThompsonDrugXA(numCells=num, rank=fac, maxit=2000, runFacts=False)
     ax[0].plot(fac_vector, sse, "r")
     xlabel = "Number of Components"
@@ -29,7 +29,9 @@ def makeFigure():
 
     rank = 3
     clust = 3
-    maximizedNK, optCP, _, x, _, _ = minimize_func(drugXA, rank=rank, n_cluster=clust)
+    maximizedNK, optCP, _, x, _, _ = minimize_func(drugXA, rank=rank, n_cluster=clust, nk_rearrange=True)
+    print(maximizedNK)
+    print(np.shape(maximizedNK))
     print("LogLik", x)
 
     ax[1].bar(np.arange(1, maximizedNK.size + 1), maximizedNK)
@@ -37,18 +39,18 @@ def makeFigure():
     ylabel = "NK Value"
     ax[1].set(xlabel=xlabel, ylabel=ylabel)
 
-    cmpCol = [f"Fac. {i}" for i in np.arange(1, fac + 1)]
-    rankCol = [f"Cmp. {i}" for i in np.arange(1, rank + 1)]
-    clustArray = [f"Clust. {i}" for i in np.arange(1, clust + 1)]
-    coords = {"Cluster": clustArray, "Factor": cmpCol, "Drug": drugXA.coords["Drug"]}
-    maximizedFactors = [
-        pd.DataFrame(optCP.factors[ii], columns=rankCol, index=coords[key])
-        for ii, key in enumerate(coords)
-    ]
-    maximizedFactors[2] = reorder_table(maximizedFactors[2])
+    # cmpCol = [f"Fac. {i}" for i in np.arange(1, fac + 1)]
+    # rankCol = [f"Cmp. {i}" for i in np.arange(1, rank + 1)]
+    # clustArray = [f"Clust. {i}" for i in np.arange(1, clust + 1)]
+    # coords = {"Cluster": clustArray, "Factor": cmpCol, "Drug": drugXA.coords["Drug"]}
+    # maximizedFactors = [
+    #     pd.DataFrame(optCP.factors[ii], columns=rankCol, index=coords[key])
+    #     for ii, key in enumerate(coords)
+    # ]
+    # maximizedFactors[2] = reorder_table(maximizedFactors[2])
 
-    for i in range(0, len(maximizedFactors)):
-        sns.heatmap(data=maximizedFactors[i], vmin=0, ax=ax[i + 2])
+    # for i in range(0, len(maximizedFactors)):
+    #     sns.heatmap(data=maximizedFactors[i], vmin=0, ax=ax[i + 2])
 
     # ranknumb = np.arange(2, 6)
     # n_cluster = np.arange(2, 6)
